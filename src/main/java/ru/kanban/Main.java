bc4c642311bade48e143b99b8f83976f3cbdb4ec
sleepd3aler <enjoydeathcore228@gmail.com>
2025-05-31 14:01:12 +0300

    24.1 Kanban part 1

--- src/main/java/ru/kanban/Main.java
+++ src/main/java/ru/kanban/Main.java
@@ -1,7 +1,24 @@
 package ru.kanban;
 
+import java.util.List;
+import ru.kanban.model.Epic;
+import ru.kanban.model.Status;
+import ru.kanban.model.Subtask;
+import ru.kanban.service.InMemoryTaskManager;
+
 public class Main {
     public static void main(String[] args) {
+        InMemoryTaskManager taskManager = new InMemoryTaskManager();
+        Epic test = new Epic("Epic1", "123", Status.NEW);
+        taskManager.addEpic(test);
+        Subtask sub1 = new Subtask("Epic1sub", "123", Status.NEW, test);
+        Subtask sub2 = new Subtask("Epic1sub2", "123", Status.NEW, test);
+        taskManager.addSubtask(sub1);
+        taskManager.addSubtask(sub2);
+        System.out.println(test);
+        taskManager.deleteSubtask(2);
+        List<Subtask> subtasks = test.getSubtasks();
+        subtasks.forEach(System.out::println);
 
     }
 }
