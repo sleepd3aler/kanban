c0d6ae7a4c6845ec3d797af19d2a48766b7482d1
sleepd3aler <enjoydeathcore228@gmail.com>
2026-01-15 00:47:33 +0300

    45.1 Kanban part 7

--- src/main/resources/db/scripts/001_ddl_create_model_tables.sql
+++ src/main/resources/db/scripts/001_ddl_create_model_tables.sql
@@ -3,8 +3,12 @@ create table if not exists tasks
     id          serial primary key,
     name        varchar(255) not null,
     description text,
-    viewed      boolean not null ,
+    viewed      boolean      not null,
     status      varchar(25)  not null CHECK ( status in ('NEW', 'IN_PROGRESS', 'DONE')),
     type        varchar(25)  not null check ( type in ('TASK', 'EPIC', 'SUBTASK')),
-    epic_id     int references tasks (id)
+    epic_id     int references tasks (id),
+    constraint check_task_type
+        check ( type = 'SUBTASK' and epic_id is not null and epic_id != id
+            or
+                type != 'SUBTASK' and epic_id is null )
 );
